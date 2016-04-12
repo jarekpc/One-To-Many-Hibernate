8f07a20782f8dd533e14f10076c1e400bd4f52e6
jarekpc <jarekpc@o2.pl>
2016-04-12 11:41:04 +0200

    add method save and ListAll

--- One-To-Many-Hibernate/src/main/java/com/jarek/zyzak/App.java
+++ One-To-Many-Hibernate/src/main/java/com/jarek/zyzak/App.java
@@ -1,8 +1,10 @@
 package com.jarek.zyzak;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -19,19 +21,36 @@ public class App
 {
     public static void main( String[] args )
     {
-    	Session session = build().openSession();
-    	
-    	Transaction transaction = session.beginTransaction();
-    	Set<Phone> phoneNumbers = new HashSet<Phone>();
+    	save();
+    	//
+    	ListAll();
+    }
+
+	private static void save() {
+		Session session = build().openSession();    
+		Transaction transaction = session.beginTransaction();
+		Set<Phone> phoneNumbers = new HashSet<Phone>();
     	phoneNumbers.add(new Phone("house", "11111111"));
     	phoneNumbers.add(new Phone("mobile","55555555"));
     	
     	Student student = new Student("Jarek", phoneNumbers);
     	session.save(student);
-    	
     	transaction.commit();
     	session.close();
-    }
+	}
+	
+	private static void ListAll(){
+		Session session = build().openSession();
+		Transaction transaction = session.beginTransaction();
+		Query query = session.createQuery("from Phone");
+    	List<Phone> phones = query.list();    	    
+    	for(Phone p: phones){
+    		System.out.println(p.getPhone_Number());
+    	}
+    	    	
+    	transaction.commit();
+    	session.close();
+	}
     
     public static SessionFactory build(){
     	SessionFactory sessionFactory;
